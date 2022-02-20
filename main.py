2edb35366b783356d90377177fd40326622005f0
Martin Urban <martin.urban@rothamsted.ac.uk>
2022-02-20 14:27:02 +0000

    add foo function

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import pandas
 import calculate
 
+def foo(x):
+  return x
+  
 print("Hello World")
 print(calculate.x)
 print("Hi Again")
 print ("Hi Again 2")
 print ("Hi Again 3")
+
+
+
+
+print(foo(20))
